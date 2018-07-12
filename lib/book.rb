006300f5c8052c1e1b15d02cbe01e68d6643a60b
Ashley Minor <aminor@kipphouston.org>
2018-07-12 17:01:28 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,16 @@
 # book.rb
 # Add you Book class here
 
-class Book
-  def initialize(title)
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
   end
 end
